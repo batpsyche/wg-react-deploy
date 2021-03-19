8ae1a9ad3bc20753298baefac67f814854b4897f
Batpsyche <35338167+batpsyche@users.noreply.github.com>
2021-03-19 22:14:10 +0530

    cors-updation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
 	async componentDidMount(){
 		const apiUrl = "http://www.mocky.io/v2/5ecb5c353000008f00ddd5a0";
 		const response = await fetch(apiUrl,{
-			mode:"cors"
+			crossDomain:true,
+			mode:"cors",
 		});
 		const data = await response.json();
 		this.setState({images:data,loading:false});
